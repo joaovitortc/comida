53f2872ddcd09b14885a0c7efbb507f924afbce7
Joao Vitor <jvtopanotti-da-cunha@myseneca.ca>
2024-10-30 17:02:55 -0400

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Nutrition Coach!</h1>
+      <p>This is a Next.js static website to log your meals and get nutritional feedback.</p>
+    </div>
+  );
+}
